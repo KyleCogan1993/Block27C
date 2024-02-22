9ea3ac51a6407b4c6c61e4c3229fa245c7b99ac1
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-02-22 15:47:15 -0500

    re-wrote Players.jsx

--- src/features/players/Players.jsx
+++ src/features/players/Players.jsx
@@ -6,27 +6,28 @@ export default function Players() {
  
     const Players = () => {
         const {data: player, error, isLoading} = useGetPlayerQuery();
+        
+        if(isLoading)
+            console.log("data is being fetched");
+        if(error)
+            console.log("error fetching data");
+        return (
+            <>
+                <div className="players">
+                    {data.data.players.map(player => (
+                        <div key={player.id} className="player-card">
+                            <div className="player-details">
+                                <h2>{player.name}</h2>
+                                <p>{player.breed}</p>
+                                <p>{player.status}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </>
+        );
     }
 
-    if(isLoading)
-        console.log("data is being fetched");
-    if(error)
-        console.log("error fetching data");
     
-    return (
-        <>
-            <div className="players">
-                {data.data.players.map(player => (
-                    <div key={player.id} className="player-card">
-                        <div className="player-details">
-                            <h2>{player.name}</h2>
-                            <p>{player.breed}</p>
-                            <p>{player.status}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </>
-    );
 }
 
