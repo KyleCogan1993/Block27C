41a18e4d9fb7c70334e8de9ed8d389f799435385
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-02-22 15:45:41 -0500

    wrote Players.jsx

--- src/app/puppySlice.js
+++ src/app/puppySlice.js
@@ -3,23 +3,27 @@ import puppyBowlApi from "../api/puppyBowlApi";
 const api = puppyBowlApi.injectEndpoints({
     endpoints: builder => ({
         getPlayer: builder.query({
-            query: () => "/players"
+            query: () => "/players",
+            invalidatesTags: ["players"]
         }),
         getPlayerId: builder.query({
-            query: (id) => "/players/" + id
+            query: (id) => "/players/" + id,
+            invalidatesTags: ["players"]
         }),
         addPlayer: builder.mutation({
             mutation: player => ({
                 url: "/players",
                 method: "POST",
                 body: player
-            })
+            }),
+            invalidatesTags: ["players"]
         }),
         deletePlayer: builder.mutation({
             mutation: id => ({
                 url: "/players/" + id,
                 method: "DELETE"
-            })
+            }),
+            invalidatesTags: ["players"]
         })
     })
 });
--- src/features/players/Players.jsx
+++ src/features/players/Players.jsx
@@ -5,12 +5,27 @@ import React from "react";
 export default function Players() {
  
     const Players = () => {
-        const {data: player} = useGetPlayerQuery();
+        const {data: player, error, isLoading} = useGetPlayerQuery();
     }
+
+    if(isLoading)
+        console.log("data is being fetched");
+    if(error)
+        console.log("error fetching data");
     
     return (
         <>
-        
+            <div className="players">
+                {data.data.players.map(player => (
+                    <div key={player.id} className="player-card">
+                        <div className="player-details">
+                            <h2>{player.name}</h2>
+                            <p>{player.breed}</p>
+                            <p>{player.status}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
